185303069f1f8cfd3f98048bef2bccf605bed6e8
CrazyHjonk <CrazyHjonk@gmail.com>
2023-12-17 12:57:13 +0100

    1.4.1 - Add config

--- src/main/java/de/strifel/VTools/VTools.java
+++ src/main/java/de/strifel/VTools/VTools.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Plugin(id = "vtools", name="VTools", version=VTools.VERSION, description="Some commands!")
 public class VTools extends VeloCrazyPlugin<VTools> {
 
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
 
     public static final TextColor COLOR_RED = TextColor.fromCSSHexString("FF5555");
     public static final TextColor COLOR_YELLOW = TextColor.fromCSSHexString("FFFF55");
---
+++ src/main/java/de/strifel/VTools/config/VToolsConfigRegistrar.java
@@ -0,0 +1,70 @@
+package de.strifel.VTools.config;
+
+import com.crazyhjonk.core.config.ConfigRegistrar;
+import com.crazyhjonk.core.config.ConfigType;
+import lombok.Getter;
+import org.jetbrains.annotations.Nullable;
+
+@Getter
+public enum VToolsConfigRegistrar implements ConfigRegistrar {
+    BROADCAST_PREFIX(String.class, "broadcast-prefix"),;
+
+    @Getter
+    public enum Section implements ConfigRegistrar.Section {
+
+        ;
+
+        private final String path;
+        private final String name;
+        private final @Nullable File file;
+
+        /**
+         * @implNote Use null for the default config file.
+         */
+        Section(String name, @Nullable File file, Section... parent) {
+            this.name = name;
+            this.file = file;
+
+            if (parent.length != 0) {
+                this.path = parent[0].getPath() + "." + name;
+            } else {
+                this.path = name;
+            }
+        }
+    }
+
+    @Getter
+    public enum File implements ConfigRegistrar.File {
+        CONFIG("config"),
+        ;
+
+        private final String name;
+
+        File(String name) {
+            this.name = name;
+        }
+    }
+
+    private final ConfigType<?> type;
+
+    private final String path;
+    private final String name;
+    private final @Nullable Section section;
+
+    VToolsConfigRegistrar(Class<?> type, String name, Section... parent) {
+        this.type = ConfigType.of(type);
+        this.name = name;
+
+        if (parent.length != 0) {
+            this.path = parent[0].getPath() + "." + name;
+            this.section = parent[0];
+            return;
+        }
+
+        this.section = null;
+        this.path = name;
+    }
+    public File getFile() {
+        return section != null ? section.getFile() : null;
+    }
+}
